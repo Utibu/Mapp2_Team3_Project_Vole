583d015da22a71bac7f57b3d13ce040e34891ccd
Helloinoit <37874965+Helloinoit@users.noreply.github.com>
2018-04-24 21:15:43 +0200

    Prototype movement

---
+++ Project Vole/Assets/PlayerInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    public float up = -0.1f;
+
+
+    private bool downMovement;
+    private Rigidbody2D rgdbd2d;
+
+    // Use this for initialization
+    void Start(){
+
+        rgdbd2d = GetComponent<Rigidbody2D>();
+
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space)) {
+
+        }
+        else
+        {
+            rgdbd2d.velocity += new Vector2(0, up);
+        }
+        
+
+
+     
+    }
+}
